4f59268832e2c2d02fee0c0fc8593411d64f981a
shafayatGit <shafayathossain.ielts@gmail.com>
2025-11-01 21:53:51 +0600

    push

--- Set.js
+++ Set.js
@@ -1,4 +1,4 @@
-// ***Features***               Set	Array                       ArrayList
+// ***Features***               Set-Array                       ArrayList(JAVA)
 // Duplicates	                ❌ Not allowed	                ✅ Allowed
 // Access by index	            ❌ No	                        ✅ Yes
 // Order preserved	            ✅ Yes (insertion order)	        ✅ Yes
@@ -7,7 +7,7 @@
 
 
 
-// ⚡ ----->>>Common Methods
+// ----->>>Common Methods
 // Method	        Description	                Example
 // add(value)	    Adds a new value	        set.add(1)
 // delete(value)	Removes a value	            set.delete(2)
@@ -31,6 +31,8 @@ console.log(set.size); // --> 5
 console.log(set.has(3)); // --> true
 console.log(set.has(6)); // --> false
 
+
+// New Set with Object References
 const nameSet = new Set();
 const Alice = { name: "Alice" };
 const Bob = { name: "Bob" };
@@ -41,11 +43,29 @@ nameSet.add(Bob);
 nameSet.add(Charlie);
 nameSet.add(Alice); // Duplicate reference, will be ignored
 
-console.log(nameSet); // --> Set(3) { { name: 'Alice' }, { name: 'Bob' }, { name: 'Charlie' } } like key-value pair
-console.log(nameSet.size); // --> 3
+
+// Iterating over Set
+nameSet.forEach((value) => { // Set does not have index, so forEach only gives value and do not return anything(undefined)
+  console.log("Looping:",value); 
+});
+
+//  Converting Set to Array
+const nameArray = Array.from(nameSet);
+console.log("After Converting",nameArray); // --> [ { name: 'Alice' }, { name: 'Bob' }, { name: 'Charlie' } ]
+console.log("Accessing By Index:", nameArray[1]); 
+
+console.log("NameSet:",nameSet); // --> Set(3) { { name: 'Alice' }, { name: 'Bob' }, { name: 'Charlie' } } like key-value pair
+console.log("Size:",nameSet.size); // --> 3
 
 nameSet.delete(Bob); // delete Value(Bob) from the Set
-console.log(nameSet); // --> Set(2) { { name: 'Alice' }, { name: 'Charlie' } }
+console.log("After Deleting Bob:",nameSet); // --> Set(2) { { name: 'Alice' }, { name: 'Charlie' } }
 
 nameSet.clear();
-console.log(nameSet); // --> Set(0) {}  
+console.log("After clearing All:",nameSet); // --> Set(0) {}  
+
+const Array1 = new Set(["Apple", "Banana", "Orange"]);
+const FruitArray = Array.from(Array1);
+FruitArray.push("Mango"); // Now we can use array methods like push
+console.log("Fruit Array:",FruitArray); // --> [ 'Apple', 'Banana', 'Orange', 'Mango' ]
+FruitArray.pop(); // Remove last element
+console.log("Fruit Array After Pop:",FruitArray); // --> [ 'Apple', 'Banana', 'Orange' ]
